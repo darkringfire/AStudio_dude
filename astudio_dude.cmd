835afebceedb74191cfe9ab752322b19986afa74
darkringfire <darkringfire@gmail.com>
2015-10-06 00:31:06 +0800

    Disable autocreate config

--- astudio_dude.cmd
+++ astudio_dude.cmd
@@ -1,6 +1,6 @@
 @echo off
 ::
-set version=AStudioDude 2015.09.24
+set version=2015.10.06
 ::
 :: astudio_dude.cmd <prodect_dir> [-c] [-f] [-e] [-F] [target_name]
 ::
@@ -36,6 +36,7 @@ set version=AStudioDude 2015.09.24
 :: [ ] Treat output as Unicode
 ::
 :: Title:              AVRdude : Reset
+:: Description         Just access to microcontroller and reset it. Without programming
 :: Command:            <path>\astudio_dude.cmd
 :: Arguments:          $(ProjectDir)
 :: Initial directory:  $(TargetDir)
@@ -55,10 +56,10 @@ set version=AStudioDude 2015.09.24
 chcp 1251 > nul
 
 echo AVRdude for Atmel Studio (%version%)
-echo ~~~~~~~~~~~~~~~~~~~~~~~~
-date /t
-time /t
-echo ~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+::date /t
+::time /t
+::echo ~~~~~~~~~~~~~~~~~~~~~~~~
 
 set editor=C:\Program Files (x86)\Notepad++\notepad++.exe
 set dudepath=%~dp0
@@ -88,20 +89,20 @@ if x%1==x-c (
     goto flags
 )
 
-if not exist "%conf%" (
-    echo !!: File %conf% not exist
+if x%Aconf%==x1 ( 
     set edit=1
     goto conf
+)
+
+if not exist "%conf%" (
+    echo !!: File %conf% not exist
+    exit
 )
 
 for /f "eol=# tokens=1,2" %%i in (%conf%) do (
     set c_%%i=%%j
 )
 
-if x%Aconf%==x1 ( 
-    set edit=1
-    goto conf
-)
 if x%c_device%==x (
     echo EE: Device not specified.
     set edit=1
@@ -126,7 +127,7 @@ set params=
 
 if x%Afuses%==x goto skipfuse
 if not x%c_writefuse%==xYES (
-    echo Get FUSEs
+    echo ii: Getting FUSEs
     for /F "tokens=*" %%i in ('"%dudepath%avrdude.exe" -p %c_device% -c %c_programmer% -U efuse:r:-:b -qq') do set c_efuse=%%i
     for /F "tokens=*" %%i in ('"%dudepath%avrdude.exe" -p %c_device% -c %c_programmer% -U hfuse:r:-:b -qq') do set c_hfuse=%%i
     for /F "tokens=*" %%i in ('"%dudepath%avrdude.exe" -p %c_device% -c %c_programmer% -U lfuse:r:-:b -qq') do set c_lfuse=%%i
